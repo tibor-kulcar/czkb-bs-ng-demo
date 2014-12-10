65f634ebb0406c7744f5751abd44697a34a9df08
stego <stegena@gmail.com>
2014-12-10 12:40:22 +0100

    karma testy

---
+++ app/assets/test/unit/dummy.js
@@ -0,0 +1,9 @@
+describe('service', function () {
+    beforeEach(angular.mock.module('czkb-bs-ng-demo', function () {
+        }
+    ))
+
+    it("Ensure right universe", function() {
+        expect(0).toEqual(1);
+    })
+});
